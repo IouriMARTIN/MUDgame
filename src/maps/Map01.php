2768d378a52ab3541d330ee635bdc006944a7b16
Matis <matisconan2006@gmail.com>
2025-09-23 16:07:28 +0200

    Load Main and Map01

---
+++ src/maps/Map01.php
@@ -0,0 +1,37 @@
+<?php 
+
+namespace Matis\MuDgame\Maps;
+use Jugid\Staurie\Component\Map\Blueprint;
+use Jugid\Staurie\Game\Position\Position;
+
+class Map01 extends Blueprint {
+  
+  private Position $position;
+  public function __construct()
+    {
+        $this->position = new Position(0,0);
+    }
+ public function npcs(): array {
+  return [];
+  }
+ public function items(): array { 
+  return [];
+ }
+ public function monsters(): array { 
+  return [];
+ }
+ public function name(): string { 
+   return 'Test map Iouri';
+ }
+ 
+ public function description(): string { 
+   return 'This is a test map';
+ }
+
+  public function position(): Position
+    {
+        return $this->position;
+    }
+
+}
+?>
