f1d450cef3c0288394e820fb33e1a4f1a72a68ac
Iouri MARTIN <iourimartin@gmail.com>
2025-09-24 13:47:45 +0200

    add items

---
+++ src/Component/Items/CaleconSale.php
@@ -0,0 +1,25 @@
+<?php
+namespace Matis\MuDgame\items; //A REMPLACER*
+
+use Jugid\Staurie\Game\Item_Equippable;
+
+class CaleconSale extends Item_Equippable {
+ public function name() : string {
+    return 'Caleçon sale';
+ }
+
+ public function description(): string {
+    return 'Il a été utilisé... Surement plusieurs fois';
+ }
+
+ public function body_part(): string {
+    return 'body';
+ }
+
+ public function statistics(): array {
+    return [
+      'DEF'=> 1
+    ];
+ }
+
+}
